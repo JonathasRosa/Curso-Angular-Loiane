08c0265d9b2a9275073e23d6fd54b7b75d1c5a8b
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-05-17 11:09:19 -0300

    Update Cursos de Angular da Loiane

--- Property/binding/src/app/data-binding/data-binding.component.ts
+++ Property/binding/src/app/data-binding/data-binding.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
   styleUrls: ['./data-binding.component.css']
 })
-export class DataBindingComponent {
+export class DataBindingComponent implements OnInit{
 
+  url: string = "https://jonathasrosa.github.io/CV-Online/";
+  cursoAngular: boolean = true;
+  urlImagem = 'http://lorempixel.com/400/200/nature/';
+
+  getValor(){
+    return 1;
+  }
+
+  getCurtirCurso(){
+return true;
+  }
+
+  constructor() {};
+
+  ngOnInit() {
+
+  };
 }
